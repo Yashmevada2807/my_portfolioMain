2ce1f43680822a387e3fb403e3dd56f8a256c30c
Yashmevada2807 <yashmevada07@gmail.com>
2025-03-01 00:36:09 +0530

    add contact.jsx file

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='border w-auto flex py-2 shadow-2xl h-[530px]'>
+      This is contact Page
+    </div>
+  )
+}
+
+export default Contact
