3ed59a831c836370f50c71acf0b6307e5b406a11
Yashmevada2807 <yashmevada07@gmail.com>
2025-03-01 17:46:49 +0530

    add Navbar file seperate

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const Navbar = () => {
+    const location = useLocation();
+    const [activeLink, setActiveLink] = useState(location.pathname);
+
+    useEffect(() => {
+        setActiveLink(location.pathname); // Update active link on route change
+    }, [location.pathname]);
+
+    return (
+        <nav className="w-full p-3 bg-black">
+            <div className="text-white flex justify-between items-center">
+                <div className="logo font-bold text-2xl px-3">Yash.me
+
+                </div>
+                  <div className="routes flex gap-6">
+                    {[
+                        {Name: "Home" , path: "/" },
+                        {Name: "AboutMe" , path: "/aboutme" },
+                        {Name: "MyProjects" , path: "/myprojects" },
+                        
+                    ].map((route,index)=>(
+                        <Link
+                        key={index}
+                        to= {route.path}
+                        
+                        className={` duration-300 ${activeLink === route.path ? "scale-115" : "scale-100"
+                            }`}
+                    >
+                        {route.Name}
+                    </Link>
+                    ))} 
+                </div> 
+                <div className="flex px-3">
+                    <Link
+                        to="/Contacts"
+                        onClick={() => setActiveLink("/Contacts")}
+                        className={`duration-300 ${activeLink === "/Contacts" ? "scale-115" : "scale-100"
+                            }`}
+                    >
+                        MyContact
+                    </Link>
+                </div>   
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
