070fb3fb5cca91eb065f63189f4556b3fc9b574a
Yashmevada2807 <yashmevada07@gmail.com>
2025-03-01 17:51:57 +0530

    removed whole navbar and footer from it and create separate files of them

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,25 @@
-import {BrowserRouter as Router ,Routes ,Route ,Link} from 'react-router-dom'
+import {BrowserRouter as Router ,Routes ,Route ,Link ,useLocation} from 'react-router-dom'
+import { useState } from 'react'
 import './App.css'
-import Home from './components/home'
+import Home from './components/Home'
 import About from './components/About'
 import MyProject from './components/MyProject'
 import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
-
   return (
     <Router>
       <>
-      <nav className='w-full p-3 shadow-2xl bg-black '>
-        <div className='text-white  flex justify-between items-center'>
-          <div className="logo font-bold text-2xl px-3">
-            Yash.me
-          </div>
-          <div className="routes flex gap-4  ">
-            <Link className='hover:scale-[110%] duration-350' to='/'>Home</Link>
-            <Link className='hover:scale-[110%] duration-350' to='/AboutMe'>AboutMe</Link>
-            <Link className='hover:scale-[105%] duration-350' to='/MyProject'>MyProject</Link>
-          </div>
-          <div className='flex'>
-            <Link className='hover:scale-[110%] duration-300' to="/Contact">MyContact</Link>
-          </div>
-        </div>
-      </nav>
-      
-
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/aboutme' element={<About />}/>
-        <Route path='/myproject' element={<MyProject />}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/myprojects' element={<MyProject />}/>
+        <Route path='/contacts' element={<Contact />}/>
       </Routes>
-      <footer className='flex justify-center my-5 items-center'>
-        all copyrights reserved
-      </footer>
+      <Footer/>
     </>
     </Router>
   )
