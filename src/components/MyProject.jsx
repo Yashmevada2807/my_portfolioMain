bd13aa942ae22835c967b6e5cb7868466401725e
Yashmevada2807 <yashmevada07@gmail.com>
2025-03-02 23:24:29 +0530

    sm changes

--- src/components/MyProject.jsx
+++ src/components/MyProject.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MyProject = () => {
   return (
     <div className=' w-auto flex py-2 shadow-2xl h-[530px]'>
-      This is project page where all the projects will show
+      
     </div>
   )
 }
