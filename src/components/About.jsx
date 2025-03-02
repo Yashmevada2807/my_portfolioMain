db32f8b2ccb655b6365e6468a37ef8f2e3cbf6a4
Yashmevada2807 <yashmevada07@gmail.com>
2025-03-02 23:23:40 +0530

    sm changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className=' w-auto flex py-2 shadow-2xl h-[530px]'>
+    <div className=' w-full flex py-2 shadow-2xl h-[530px]'>
       This is About Page
     </div>
   )
