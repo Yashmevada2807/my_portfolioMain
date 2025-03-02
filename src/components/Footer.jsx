0acd6da54ed39df058463c8ad3ee1bc9bc6e9749
Yashmevada2807 <yashmevada07@gmail.com>
2025-03-02 23:23:58 +0530

    sm changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='w-full  my-5 flex relative overflow-hidden'>
-            <div className="py-4 w-full flex justify-center  animate-marquee">
+        <footer className='w-full  flex relative overflow-hidden'>
+            <div className="py-4  w-full flex justify-center  animate-marquee">
                 {["HTML",
                     "CSS",
                     "JavaScript",
                     "Tailwind CSS",
                     "React.js",
+                    "Vercel",
                     "Node.js",
                     "Express.js",
                     "MongoDB",
                     "C++",
                     "Unity Engine",
                     "Unreal Engine",].map((tech, index) => (
-                    <span key={index} className="mx-4 border-[1px] px-4 rounded-sm">
+                    <span key={index} className="mx-5 border-[1px] flex items-center px-5 text-center rounded-sm">
                         {tech}
                     </span>
                 ))}
