47ad6f9dba1a7d1d6f7c46991ad421067302f4a9
Yashmevada2807 <yashmevada07@gmail.com>
2025-03-03 21:18:17 +0530

    sm changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 const Navbar = () => {
     const location = useLocation();
     const [activeLink, setActiveLink] = useState(location.pathname);
+    const [isOpen, setIsOpen] = useState(false)
 
     useEffect(() => {
         setActiveLink(location.pathname); // Update active link on route change
@@ -13,26 +14,26 @@ const Navbar = () => {
         <nav className=" p-3 bg-black">
             <div className="text-white flex justify-between items-center">
                 <div className="logo w-full sm:w-auto flex  justify-center sm:justify-start  font-bold text-2xl px-3">Yash.me
-
                 </div>
-                  <div className=" flex max-sm:hidden  text-white  gap-x-5 p-1 ">
+                {/* for medium and large screen */}
+                <div className=" flex max-sm:hidden  text-white  gap-x-5 p-1 ">
                     {[
-                        {Name: "Home" , path: "/" },
-                        {Name: "AboutMe" , path: "/aboutme" },
-                        {Name: "MyProjects" , path: "/myprojects" },
-                        
-                    ].map((route,index)=>(
+                        { Name: "Home", path: "/" },
+                        { Name: "AboutMe", path: "/aboutme" },
+                        { Name: "MyProjects", path: "/myprojects" },
+
+                    ].map((route, index) => (
                         <Link
-                        key={index}
-                        to= {route.path}
-                        
-                        className={` duration-300 ${activeLink === route.path ? "scale-115" : "scale-100"
-                            }`}
-                    >
-                        {route.Name}
-                    </Link>
-                    ))} 
-                </div> 
+                            key={index}
+                            to={route.path}
+
+                            className={` duration-300 ${activeLink === route.path ? "scale-115" : "scale-100"
+                                }`}
+                        >
+                            {route.Name}
+                        </Link>
+                    ))}
+                </div>
                 <div className=" max-sm:hidden px-3">
                     <Link
                         to="/Contacts"
@@ -42,7 +43,21 @@ const Navbar = () => {
                     >
                         MyContact
                     </Link>
-                </div>   
+                </div>
+                {/* For Mobile Screen */}
+                <button className="sm:hidden text-2xl cursor-pointer hover:scale-110 duration-150" onClick={() =>setIsOpen(!isOpen) }>
+                ☰
+                </button>
+
+                {/* for hamburger functionality  */}
+                {isOpen && (
+                    <div className="absolute top-14 border-t right-0 flex flex-col w-full bg-black text-center items-center duration-300 sm:hidden ">
+                        <Link to="/" className="py-2 text-white hover:text-gray-400 border-b-[1px] w-full  "onClick={() => setIsOpen(false)} >Home</Link>
+                        <Link to="/aboutme" className="py-2 text-white hover:text-gray-400 border-b-[1px] w-full"onClick={() => setIsOpen(false)} >AboutMe</Link>
+                        <Link to="/myprojects" className="py-2 text-white hover:text-gray-400 border-b-[1px] w-full" onClick={() => setIsOpen(false)}>MyProjects</Link>
+                        <Link to="/contacts" className="py-2 text-white hover:text-gray-400 border-b-[1px] w-full" onClick={() => setIsOpen(false)}>ContactMe</Link>
+                    </div>
+                )}
             </div>
         </nav>
     );
